eddee5493302f5baf589bf143d2d1d310e606ec6
Benrise <benrise_x@mail.ru>
2023-05-11 05:42:15 +0300

    Categories v2, adaptive added

--- js/main.js
+++ js/main.js
@@ -6,4 +6,53 @@ if (isMobile.any()){
 }
 burgerInit();
 
-new Swiper('.slider__container');
+new Swiper('.swiper-container',{
+
+
+    navigation:{
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    slidesPerView: 6,
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+    },
+
+    mousewheel:{
+        sensivity: 1
+    },
+
+    spaceBetween: 1,
+
+    autoplay: {
+        delay: 3000,
+
+        disableOnInteraction:false,
+    },
+
+    speed: 1000,
+
+    breakpoints: {
+        280:{
+            slidesPerView: 1
+        },
+        320: {
+            slidesPerView: 2
+        },
+        580: {
+            slidesPerView: 3,
+        },
+        1100:{
+            slidesPerView: 6
+        }
+        
+    }
+});
